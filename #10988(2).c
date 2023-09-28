0dd47a79674025d71c64c43efc549bd74414aac7
gureum <68941460+ddalgiwoouuuuu@users.noreply.github.com>
2023-09-28 19:34:00 +0900

    Update #10988(2).c

--- #10988(2).c
+++ #10988(2).c
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> // bool 타입을 사용하기 위해 추가
 
 #define MAX_LEGNTH 100
 
 int main() {
 	char str[MAX_LEGNTH];
-	bool result = true;
+	bool result = true; // bool을 사용하고 true를 1 대신 사용
 
 	scanf("%s", str);
 
@@ -23,3 +23,7 @@ int main() {
 
 	return 0;
 }
+/*result 변수는 불리언(bool) 타입으로 선언되어 있으며, 
+초기값으로 true를 가지고 시작. 이후에 코드에서 문자열을 검사하면서, 
+만약 문자열이 팰린드롬이 아니라면 result 변수를 false로 변경.
+*/
